fafbb1e9d3c6e945dc8af2082cbcbbbf8d259d78
Viacheslav Ptsarev <leemour@gmail.com>
2019-05-13 11:12:53 +0300

    Add app generator

---
+++ lib/hanami_id/commands/auth.rb
@@ -0,0 +1,46 @@
+require 'optparse'
+require_relative "../generators/app"
+
+
+options = {}
+optparse = OptionParser.new do |opts|
+  opts.banner =
+    <<~DESC
+      \nHanami authorization application generator
+      Usage: hanami g auth --app auth --model user
+      Flags:
+    DESC
+
+  opts.on("-a", "--app APP", "Specify the application name") do |app_name|
+    options[:app] = app_name
+  end
+
+  opts.on("-m", "--model MODEL", "Specify the model name") do |policy|
+    options[:model] = policy
+  end
+
+  opts.on("-h", "--help", "Displays help") do
+    puts opts
+    exit
+  end
+end
+
+
+begin
+  optparse.parse!
+  puts "Add flag -h or --help to see usage instructions." if options.empty?
+  mandatory = [:app, :model]
+  missing = mandatory.select { |arg| options[arg].nil? }
+  unless missing.empty?
+    raise OptionParser::MissingArgument.new(missing.join(', '))
+  end
+
+rescue OptionParser::InvalidOption, OptionParser::MissingArgument
+  puts optparse
+  puts $ERROR_INFO.to_s
+  exit
+end
+
+puts "Generating with options: #{options.inspect}"
+HanamiId::Genertors::App.generate(options[:app], options[:model])
+puts "Generated successfully"
